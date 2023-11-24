09bd05c91072449cd189957f79c972d22b0ca920
Razvan <razvan.brailoiu@student.upt.ro>
2023-11-24 20:01:52 +0200

    added V1 of consumer and producer architecture

---
+++ StockExchangeProject/src/StockMarket/KafkaProducerConfig.java
@@ -0,0 +1,24 @@
+package StockMarket;
+
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class KafkaProducerConfig {
+    public static KafkaProducer<String, String> createProducer() {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        return new KafkaProducer<>(properties);
+    }
+
+    public static void sendMessage(KafkaProducer<String,String> producer, String topic, String message) {
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, message);
+        producer.send(record);
+    }
+}
--- StockExchangeProject/src/StockMarket/Main.java
+++ StockExchangeProject/src/StockMarket/Main.java
@@ -15,14 +15,14 @@ public class Main {
         Collection<Thread> threads = new ArrayList<>();
 
         for (int i = 0; i < numberOfBuyers; i++){
-            Thread buyerThread = new Thread(new Buyer(stockMarketSimulation, "AAPL", 100, 44.6, 1234, 0));
+            Thread buyerThread = new Thread(new Buyer(stockMarketSimulation, "AAPL", 10, 44.6, 1234, 0));
             buyerThread.setName("Buyer " + i);
             buyerThread.start();
             threads.add(buyerThread);
         }
 
         for (int i = 0 ; i < numberOfSellers; i++){
-            Thread sellerThread = new Thread(new Seller(stockMarketSimulation, "AAPL", 50, 44.6, 1234, 0));
+            Thread sellerThread = new Thread(new Seller(stockMarketSimulation, "MSFT", 50, 56.7, 1234, 0));
             sellerThread.setName("Seller " + i);
             sellerThread.start();
             threads.add(sellerThread);
@@ -36,7 +36,7 @@ public class Main {
             }
         }
 
-
+        stockMarketSimulation.closeChannel();
         Collection<Stock> stocks = stockMarketSimulation.getAllStocks();
         for (var stock : stocks){
             System.out.println("Remaining quantity for " + stock.getSymbol() + " : " + stock.getQuantity());
